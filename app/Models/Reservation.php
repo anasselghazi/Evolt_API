95fe117099cda4688fce26e1537190c96c4ecbe1
anasselghazi <anasseelghazi10@gmail.com>
2026-03-12 13:50:54 +0000

    feat:ajouter les relation entre les class reservation et user et charginStation

--- app/Models/Reservation.php
+++ app/Models/Reservation.php
@@ -13,4 +13,8 @@ class Reservation extends Model
         'heure',
         'status',
     ];
+    public function chargingStation()
+    {
+    return $this->belongsTo(ChargingStation::class);
+    }
 }
