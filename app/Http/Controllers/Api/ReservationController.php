98fdc178b77278f5eb4db2f321552face90d3fc3
anasselghazi <anasseelghazi10@gmail.com>
2026-03-12 09:40:11 +0000

    feat:ajouter la partie reservation dans le project

---
+++ app/Http/Controllers/Api/ReservationController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Reservation;
+
+class ReservationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+
+
+        $reservation = Reservation::create([
+            'user_id' => auth()->id(),  
+            'charging_station_id' => $request->charging_station_id,
+            'date' => $request->date,
+            'heure' => $request->heure,
+            'status' => 'pending',    
+        ]);
+
+        return response()->json($reservation, 201);
+    
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,15 @@
 <?php
-
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\ReservationController;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/reservation', [ReservationController::class, 'store']);
+});
